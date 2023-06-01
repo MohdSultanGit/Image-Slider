1c76a1bf62211ebb7489a514c1731aeb88c29673
Rana Yalcin <yalcinrana06@gmail.com>
2023-06-01 15:59:28 +0300

    fixed last image problem

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const slides = document.querySelectorAll(".slide")
 var counter = 0;
-// console.log(slides)
+//console.log(slides)
 slides.forEach(
     (slide, index) => {
         slide.style.top = `${index * 100}%`
@@ -13,14 +13,19 @@ const goPrev = () => {
 }
 
 const goNext = () => {
-    counter++
+    counter++ 
     slideImage()
 }
 
 const slideImage = () => {
     slides.forEach(
         (slide) => {
+            if (counter === slides.length) {
+                counter = 0;
+            }
             slide.style.transform = `translateY(-${counter * 100}%)`
+            console.log(slide.style.transform)
         }
+
     )
 }
